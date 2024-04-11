b45dc88407804efd129285b75b59ea307b819091
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-11 04:27:49 +0300

    Added Password Generator script

---
+++ Password Generator/README.md
@@ -0,0 +1,13 @@
+<div align="center">
+
+# Password Generator
+
+</div>
+
+### Description:
+This Python script generates a random password of a specified length with a combination of letters, numbers, and special characters.
+
+### How to Use:
+1. Run the script in a Python environment.
+2. Enter the desired length of the password when prompted.
+3. The script will generate a random password of the specified length and display it.
---
+++ Password Generator/password-generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+def main():
+    length = int(input("Enter the length of the password: "))
+    password = generate_password(length)
+    print("Generated Password:", password)
+
+
+if __name__ == "__main__":
+    main()
