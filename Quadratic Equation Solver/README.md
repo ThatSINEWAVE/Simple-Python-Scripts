8ca375f87fd207bc20752bdba775795901ba7239
Ahmed Afif Al Raihan <alraihanahmedafif1@gmai.com>
2026-03-29 22:44:41 +0600

    feat: handle zero-integer coefficients and add complex root support

--- Quadratic Equation Solver/README.md
+++ Quadratic Equation Solver/README.md
@@ -5,9 +5,9 @@
 </div>
 
 ### Description:
-This Python script solves quadratic equations of the form ax^2 + bx + c = 0 and displays the roots.
+This Python script solves quadratic equations of the form ax² + bx + c = 0 and displays the roots.
 
 ### How to Use:
 1. Run the script in a Python environment.
 2. Enter the coefficients a, b, and c of the quadratic equation when prompted.
-3. The script will calculate and display the roots of the quadratic equation.
+3. The script will calculate the roots of the quadratic equation.
--- Quadratic Equation Solver/quadratic-equations.py
+++ Quadratic Equation Solver/quadratic-equations.py
@@ -1,27 +1,36 @@
 import math
 
+plus_minus = "\u00B1"
 
 def solve_quadratic(a, b, c):
     discriminant = b**2 - 4*a*c
-    if discriminant > 0:
-        root1 = (-b + math.sqrt(discriminant)) / (2*a)
-        root2 = (-b - math.sqrt(discriminant)) / (2*a)
-        return root1, root2
-    elif discriminant == 0:
-        root = -b / (2*a)
-        return root, root
-    else:
-        return "No real roots"
 
+    if a == 0 and b != 0:
+        x = -c/b
+        return x
+    elif a == 0 and b == 0 and c == 0:
+        return "infinite solutions"
+    elif a == 0 and b == 0 and c != 0:
+        return "no solutions"
+    else:
+        if discriminant > 0:
+            root1 = (-b + math.sqrt(discriminant)) / (2*a)
+            root2 = (-b - math.sqrt(discriminant)) / (2*a)
+            return f"Roots: {root1, root2}"
+        elif discriminant == 0:
+            root = -b / (2*a)
+            return f"Roots: {root}"
+        else:
+            positive_number = abs(discriminant)
+            complex_form = f"({-b}{plus_minus}{math.sqrt(positive_number)}i)/{2*a}"
+            return f"No real roots\n{complex_form}"
 
 def main():
     a = float(input("Enter the coefficient a: "))
     b = float(input("Enter the coefficient b: "))
     c = float(input("Enter the coefficient c: "))
-    map = f"{a}x^2 + {b}x + {c} = 0"
-    print(map)
     roots = solve_quadratic(a, b, c)
-    print("Roots:", roots)
+    print(f"{roots}")  #Output
 
 
 if __name__ == "__main__":
